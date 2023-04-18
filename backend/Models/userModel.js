936a3cb8123c5db62dce0a1db5ec4796e8107731
Shahid321fw11 <sidansari20@gmail.com>
2023-04-19 00:01:03 +0530

    created backend schema for chat message and user

---
+++ backend/Models/userModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: { type: "String", required: true },
+    email: { type: "String", unique: true, required: true },
+    password: { type: "String", required: true },
+    pic: {
+      type: "String",
+      required: true,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+  },
+  { timestaps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
