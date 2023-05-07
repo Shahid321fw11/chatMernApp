2edbe45f313d1ae71646f585d9ee25f92c237922
Shahid321fw11 <sidansari20@gmail.com>
2023-05-07 11:38:19 +0530

    changes

--- frontend/src/Pages/ChatPage.js
+++ frontend/src/Pages/ChatPage.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Box } from "@chakra-ui/layout";
 import { useState } from "react";
-// import Chatbox from "../components/Chatbox";
-// import MyChats from "../components/MyChats";
 import SideDrawer from "../Components/miscellaneous/SideDrawer";
 import { ChatState } from "../Context/ChatProvider";
+import Chatbox from "../Components/Chatbox";
+import MyChats from "../Components/MyChats";
 
 const ChatPage = () => {
   const [fetchAgain, setFetchAgain] = useState(false);
@@ -12,12 +12,12 @@ const ChatPage = () => {
   return (
     <div style={{ width: "100%" }}>
     {user && <SideDrawer />}
-    {/* <Box d="flex" justifyContent="space-between" w="100%" h="91.5vh" p="10px">
-      {user && <MyChats fetchAgain={fetchAgain} />}
-      {user && (
-        <Chatbox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
-      )}
-    </Box> */}
+    <Box display="flex" justifyContent="space-between" w="100%" h="91.5vh" p="10px">
+      { <MyChats />}
+      {
+        <Chatbox/>
+      }
+    </Box>
   </div>
   );
 };
--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -31,7 +31,7 @@ const HomePage = () => {
   return (
     <Container maxW="xl" centerContent>
       <Box
-        d="flex"
+        display="flex"
         justifyContent="center"
         p={3}
         bg="white"
